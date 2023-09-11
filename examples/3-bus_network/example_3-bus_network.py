0df58b16bfca083b829434447e9b3c1a72789f36
Lukas B. Nordentoft <Lukasbnordentoft@gmail.com>
2023-09-11 10:09:02 +0200

    Update example_3-bus_network.py

--- examples/3-bus_network/example_3-bus_network.py
+++ examples/3-bus_network/example_3-bus_network.py
@@ -10,6 +10,13 @@ Description:
     Exmple use of PyMGA to explore a network with 3 buses.
 """
 
+import os
+import sys
+
+# Add parent folder to directory to load PyMGA package
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.append(parent_dir)
+
 import PyMGA
 from PyMGA.utilities.plot import near_optimal_space_2D, near_optimal_space_matrix
 import numpy as np
@@ -62,13 +69,15 @@ if __name__ == '__main__':
     # PyMGA: Search near-optimal space using chosen method
     verticies, directions, _, _ = method.search_directions(14, n_workers = 16)
     
+    
+#%%
     # PyMGA: Sample the identified near-optimal space
     # Hit-and-run sampler, valid for all dimensions
     har_samples = PyMGA.sampler.har_sample(1000_000, x0 = np.zeros(len(variables.keys())), 
                                             directions = directions, 
                                             verticies = verticies)
     # Bayesian bootstrap sampler, good up to aorund 8 dimensions
-    bayesian_samples = PyMGA.sampler.bayesian_sample(verticies, 1_000_000) 
+    bayesian_samples = PyMGA.sampler.bayesian_sample(1_000_000, verticies) 
 
 
     #### Processing results ####
@@ -91,4 +100,70 @@ if __name__ == '__main__':
     near_optimal_space_matrix(all_variables, verticies, bayesian_samples,
                               opt_solution = opt_sol,
                               title = 'Near-optimal space')
-     
+    
+    #%%
+
+import numpy as np
+
+# Define your convex polytope by specifying its vertices and associated directions.
+# vertices = np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0], [0, 0, 1]])
+# directions = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1], [-1, 0, 0], [0, -1, 0], [0, 0, -1]])
+
+vertices = verticies
+
+# Define the number of samples you want to generate.
+num_samples = 1000
+
+# Initialize an empty list to store the generated samples.
+samples = []
+
+# Perform hit-and-run sampling
+for _ in range(num_samples):
+    # Choose a random vertex as the starting point
+    start_vertex = np.random.choice(len(vertices))
+    current_point = vertices[start_vertex]
+
+    while True:
+        # Select a random direction
+        direction_index = np.random.choice(len(directions))
+        direction = directions[direction_index]
+
+        # Calculate the distance to the nearest boundary
+        lambdas = []
+        for i in range(len(vertices)):
+            vertex = vertices[i]
+            edge = vertices[(i + 1) % len(vertices)] - vertex
+
+            # Calculate the intersection point
+            numerator = np.dot((vertex - current_point), direction)
+            denominator = np.dot(edge, direction)
+
+            if denominator == 0:
+                lambda_i = float('inf')
+            else:
+                lambda_i = numerator / denominator
+
+            if lambda_i > 0:
+                lambdas.append(lambda_i)
+
+        if not lambdas:
+            break
+
+        # Move along the selected direction by the minimum lambda
+        min_lambda = min(lambdas)
+        current_point += min_lambda * direction
+
+    # Append the current sample point to the list
+    samples.append(current_point)
+    print('Sample obtained')
+
+# Convert the list of samples to a numpy array
+samples = np.array(samples)
+
+# The 'samples' numpy array now contains randomly generated points within the convex polytope in N-dimensional space.
+
+
+#%%
+near_optimal_space_matrix(all_variables, verticies, samples,
+                          opt_solution = opt_sol,
+                          title = 'Near-optimal space')
