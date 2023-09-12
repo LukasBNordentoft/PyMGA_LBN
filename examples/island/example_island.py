811d56ec8d95cf3f33e4138120a7479323871d81
Lukas B. Nordentoft <Lukasbnordentoft@gmail.com>
2023-09-12 17:19:02 +0200

    Added partial mga constraints

--- examples/island/example_island.py
+++ examples/island/example_island.py
@@ -12,11 +12,19 @@ Description:
     This example include custom constraints being defined using extra_func.
 """
 
+import os
+import sys
+
+# Add parent folder to directory to load PyMGA package
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.append(parent_dir)
+
 import PyMGA
 from PyMGA.utilities.plot import near_optimal_space_2D, near_optimal_space_matrix
 import numpy as np
 import yaml
 import pandas as pd
+import pypsa
 
 
 if __name__ == '__main__':
@@ -53,7 +61,7 @@ if __name__ == '__main__':
     
     
     # Define constraints to be passed to extra_functionalities in n.lopf()
-    def extra_func(n, snapshots):
+    def extra_func(n, snapshots, mga_options):
         
         ### Define custom constraints
         def link_constraint(n):
@@ -141,11 +149,107 @@ if __name__ == '__main__':
             # Define constraint
             define_constraints(n, lhs, '<=', rhs, 'Island', 'Area_Use')
         
+        # Define partial MGA constraints
+        if mga_options is not None:
+            if mga_options['mga_slack'] is not None:
+                from pypsa.linopt import get_var, linexpr, join_exprs, define_constraints
+                
+                # Get MGA slack
+                epsilon = mga_options['mga_slack']
+                
+                # # Get optimum system
+                # n_opt = pypsa.Network('n_opt.nc')
+                
+                ### Create partial mga constraint for local demand
+                # Loop through optimal system to find cost of local variables
+                # local_cost = 0
+                # for variable in ['Data', 'P2X', 'Storage']:
+                #     if variable == 'Storage': #and not n_opt.stores.e_nom_opt[variable] == 0:
+                        
+                #         capital_cost  = n_opt.stores.capital_cost[variable]
+                #         e_nom         = n_opt.stores.e_nom_opt[variable]
+                #         marginal_cost = n_opt.stores.marginal_cost[variable]
+                #         e_sum         = n_opt.stores_t.e[variable].sum()
+                        
+                #         local_cost += capital_cost * e_nom + marginal_cost * e_sum
+
+                #     elif variable == 'Data' or variable == 'P2X':
+                #         #capital costs
+                #         capital_cost  = n_opt.generators.capital_cost[variable]
+                #         p_nom         = n_opt.generators.p_nom_opt[variable]
+                        
+                #         # marginal revenues
+                #         marginal_cost = n_opt.generators.marginal_cost[variable]
+                #         p_sum         = n_opt.generators_t.p[variable].sum()
+
+                #         local_cost += capital_cost * p_nom + marginal_cost * p_sum
+                        
+                
+                # Get variables
+                vars_gen     = get_var(n, 'Generator', 'p_nom')
+                vars_gen_t   = get_var(n, 'Generator', 'p')
+                vars_store   = get_var(n, 'Store', 'e_nom')
+                vars_store_t = get_var(n, 'Store', 'e')
+                
+                # Multiply each variable with matching capital cost and add it all together
+                # l_cc = linexpr( (n_opt.generators.capital_cost['P2X'],  vars_gen["P2X"]), 
+                #                 (n_opt.generators.capital_cost['Data'], vars_gen["Data"]), 
+                #                 (n_opt.stores.capital_cost['Storage'],  vars_store['Storage']) )
+                
+                # l_mc_store = linexpr( (n_opt.stores.marginal_cost['Storage'], vars_store_t['Storage']) ).sum()
+                # l_mr_data  = linexpr( (n_opt.generators.marginal_cost['Data'], vars_gen_t['Data']) ).sum()
+                # l_mr_p2x   = linexpr( (n_opt.generators.marginal_cost['P2X'], vars_gen_t['P2X']) ).sum()
+                
+                # Multiply each variable with matching capital cost and add it all together
+                # l_cc = linexpr( (39912.553193184336,  vars_gen["P2X"]), 
+                #                 (9453203.316547655, vars_gen["Data"]), 
+                #                 (53914.141711254,  vars_store['Storage']) )
+                
+                # # l_cc_store = linexpr( (53914.141711254,    vars_store['Storage']) )
+                # l_cc_data  = linexpr( (9453203.316547655,  vars_gen["Data"]) )
+                # r_cc_data  = 9453203.316547655 * 2197.7980424170364 * (1+epsilon)
+                
+                # l_cc_p2x   = linexpr( (39912.553193184336, vars_gen["P2X"])  )
+                # r_cc_data  = 39912.553193184336 * 2197.7980424170364 * (1+epsilon)
+                
+                # # l_mc_store = linexpr( (1.8, vars_store_t['Storage']) ).sum()
+                # # l_mr_data  = linexpr( (2023.6, vars_gen_t['Data']) ).sum()
+                # # l_mr_p2x   = linexpr( (42.6, vars_gen_t['P2X']) ).sum()
+                
+                lhs = ( linexpr( (1, vars_gen['P2X']) ) 
+                       + linexpr( (1, vars_store['Storage']) )
+                       )
+                
+                define_constraints(n, lhs, "<=", 1000, "GlobalConstraint", "Combine_test")
+                
+                # # Define lhs
+                # lhs = l_cc + l_mc_store + l_mr_data + l_mr_p2x
+                
+                # # Define rhs
+                # rhs = local_cost + abs(local_cost) * (epsilon)
+                
+                # define_constraints(n, lhs, "<=", rhs, "GlobalConstraint", "local_mga_constraint")
+        
         ### Call custom constraints 
         link_constraint(n)
         marry_links(n)
         area_constraint(n)
-
+        
+        from pypsa.linopt import get_var, linexpr, join_exprs, define_constraints
+        vars_gen     = get_var(n, 'Generator', 'p_nom')
+        vars_gen_t   = get_var(n, 'Generator', 'p')
+        vars_store   = get_var(n, 'Store', 'e_nom')
+        vars_store_t = get_var(n, 'Store', 'e')
+        
+        # lhs = (   linexpr( (1, vars_gen_t['Data']) ).sum() 
+        #         + linexpr( (1, vars_gen_t['P2X']) ).sum()
+        #         )
+        
+        # define_constraints(n, lhs, ">=", -19000000, "GlobalConstraint", "Combine_test_sum")
+        
+        lhs = linexpr( (2023.6, vars_gen_t['Data']) ).sum()
+        rhs = -19000000 * 2023.6 
+        define_constraints(n, lhs, ">=", rhs, "GlobalConstraint", "Combine_test_sum")
 
 
     #### PyMGA ####
@@ -161,30 +265,37 @@ if __name__ == '__main__':
     method = PyMGA.methods.MAA(case)
     
     # PyMGA: Solve optimal system
-    opt_sol, obj, n_solved = method.find_optimum()
+    opt_sol, obj, n_opt = method.find_optimum()
+    n_opt.export_to_netcdf('n_opt.nc')
     
     # Draw optimal system (optional)
     # draw_network(n_solved, show_capacities = True)
     
     # PyMGA: Search near-optimal space using chosen method
-    verticies, directions, _, _ = method.search_directions(14, n_workers = 16)
+    # verticies, directions, _, _ = method.search_directions(14, n_workers = 16)
 
-    # PyMGA: Sample the identified near-optimal space
-    MAA_samples = PyMGA.sampler.har_sample(100_000, x0 = np.zeros(len(variables.keys())), 
-                                            directions = directions, 
-                                            verticies = verticies)
-
-
-    #### Process results ####
-    # Plot near-optimal space of Data (x1) and P2X (x2)
-    all_variables    = list(variables.keys())
-    chosen_variables = ['x1', 'x2']
-    near_optimal_space_2D(all_variables, chosen_variables,
-                          verticies, MAA_samples,
-                          plot_MAA_points = True,
-                          bins = 50)
+    # # PyMGA: Sample the identified near-optimal space
+    # har_samples = PyMGA.sampler.har_sample(1_000_000, x0 = np.zeros(len(variables.keys())), 
+    #                                         directions = directions, 
+    #                                         verticies = verticies)
     
-    # Matrix plot of 2D "sides" of polytope, with histograms and correlations
-    near_optimal_space_matrix(all_variables, verticies, MAA_samples,
-                              xlabel = 'Unit []', ylabel = 'Unit []',
-                              opt_solution = opt_sol)
+    # bayesian_samples = PyMGA.sampler.bayesian_sample(1_000_000, verticies)
+
+
+    # #### Process results ####
+    # # Plot near-optimal space of Data (x1) and P2X (x2)
+    # all_variables    = list(variables.keys())
+    # chosen_variables = ['x1', 'x2']
+    # # near_optimal_space_2D(all_variables, chosen_variables,
+    # #                       verticies, MAA_samples,
+    # #                       plot_MAA_points = True,
+    # #                       bins = 50)
+    
+    # # Matrix plot of 2D "sides" of polytope, with histograms and correlations
+    # near_optimal_space_matrix(all_variables, verticies, har_samples,
+    #                           xlabel = 'Unit []', ylabel = 'Unit []',
+    #                           opt_solution = opt_sol)
+    
+    # near_optimal_space_matrix(all_variables, verticies, bayesian_samples,
+    #                           xlabel = 'Unit []', ylabel = 'Unit []',
+    #                           opt_solution = opt_sol)
